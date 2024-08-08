5c7206b064afb145d3ffe67cef105bf5fe100a0f
Lenni009 <guentherhanslphdyt@gmail.com>
2024-08-08 20:54:36 +0200

    add save name to overview

--- src/pages/GeneralStats.vue
+++ src/pages/GeneralStats.vue
@@ -8,9 +8,15 @@ const saveDataStore = useSaveDataStore();
 const { data } = storeToRefs(saveDataStore);
 
 const playTime = computed(() => formatTime(data.value?.CommonStateData.TotalPlayTime));
+const saveName = computed(() => data.value?.CommonStateData.SaveName);
 </script>
 
 <template>
-  <div>Playtime:</div>
-  <div>{{ playTime }}</div>
+  <div>
+    <span>Name:</span> <span>{{ saveName }}</span>
+  </div>
+
+  <div>
+    <span>Playtime:</span> <span>{{ playTime }}</span>
+  </div>
 </template>
